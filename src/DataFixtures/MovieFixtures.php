91d72f7b6e21668e26dad4468f2c2ba1fc91d329
Marcus Reis <mvsantosreis@gmail.com>
2024-04-09 11:28:56 +0100

    Data Fixures Entity References

--- src/DataFixtures/MovieFixtures.php
+++ src/DataFixtures/MovieFixtures.php
@@ -15,6 +15,9 @@ class MovieFixtures extends Fixture
         $movie->setReleaseYear(2008);
         $movie->setDescription('This is the description of the Dark Knight');
         $movie->setImagePath('https://cdn.pixabay.com/photo/2024/01/15/11/36/batman-8510022_960_720.png');
+        
+        $movie->addActor($this->getReference('actor_1'));
+        $movie->addActor($this->getReference('actor_2'));
         $manager->persist($movie);
 
         $movie2 = new Movie();
@@ -22,6 +25,9 @@ class MovieFixtures extends Fixture
         $movie2->setReleaseYear(2019);
         $movie2->setDescription('This is the description of the Avengers: Endgame');
         $movie2->setImagePath('https://cdn.pixabay.com/photo/2022/06/05/11/06/action-figures-7243788_1280.jpg');
+        
+        $movie->addActor($this->getReference('actor_3'));
+        $movie->addActor($this->getReference('actor_4'));
         $manager->persist($movie2);
 
         $manager->flush();
